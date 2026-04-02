bd8d424a4f5b8d8bc9afbad6dac9a3d67b6c9814
Shamroz Abrar <shamrozahmad2412@gmail.com>
2026-04-02 16:02:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -469,9 +469,7 @@ components.html("""
     position: fixed;
     top: 0;
     left: 0;
-    width: 100vw;
-    height: 100vh;
-    overflow: hidden;
+    z-index: 0;   /* 👈 IMPORTANT */
 }
 
     background:
